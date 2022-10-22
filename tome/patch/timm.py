5e9489f1ad28f6951796bda41ab7fefba68af1c9
Merkurev Maxim <96414688+kos94ok@users.noreply.github.com>
2022-10-22 23:36:09 +0500

    Update timm.py

--- tome/patch/timm.py
+++ tome/patch/timm.py
@@ -26,13 +26,12 @@ class ToMeBlock(Block):
      - Compute and propogate token size and potentially the token sources.
     """
 
-    def forward(self, x: torch.Tensor, drop_path=0.) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
 
-        self.drop_path_rate = DropPath(drop_path) if drop_path > 0. else nn.Identity()  
         # Note: this is copied from timm.models.vision_transformer.Block with modifications.
         attn_size = self._tome_info["size"] if self._tome_info["prop_attn"] else None
         x_attn, metric = self.attn(self.norm1(x), attn_size)
-        x = x + self.drop_path_rate(x_attn)
+        x = x + self.drop_path1(x_attn)
 
         r = self._tome_info["r"].pop(0)
         if r > 0:
@@ -49,7 +48,7 @@ class ToMeBlock(Block):
                 )
             x, self._tome_info["size"] = merge_wavg(merge, x, self._tome_info["size"])
 
-        x = x + self.drop_path_rate(self.mlp(self.norm2(x)))
+        x = x + self.drop_path1(self.mlp(self.norm2(x)))
         return x
 
 
